6720c87d7e294e0f6a97266ff16cca3c8f2a9583
jr <fernando.tabamo@whitecobalt.com>
2024-11-17 23:55:00 +0800

    fix bug overflow

--- app/(menu)/settings.tsx
+++ app/(menu)/settings.tsx
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native"
+import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity } from "react-native"
 import { ThemedText } from "@/components/ThemedText"
 import { ThemedView } from "@/components/ThemedView"
 import { useSettingsStore } from "@/store/settings"
@@ -72,59 +72,61 @@ export default function TabTwoScreen() {
         </>
       )}
     >
-      <ThemedView colorName="card" style={[styles.exampleContainer]}>
-        <ThemedText style={{ fontSize: previewFontSize, lineHeight: Math.max(previewFontSize * 1.5, 24) }}>
-          <Text>1.</Text> {previewVerse}
+      <ScrollView style={{flex: 1}}>
+        <ThemedView colorName="card" style={[styles.exampleContainer]}>
+          <ThemedText style={{ fontSize: previewFontSize, lineHeight: Math.max(previewFontSize * 1.5, 24) }}>
+            <Text>1.</Text> {previewVerse}
+          </ThemedText>
+        </ThemedView>
+        <ThemedText>
+          Laki ng mga Titik (Font Size): {previewFontSize?.toFixed(0)}
         </ThemedText>
-      </ThemedView>
-      <ThemedText>
-        Laki ng mga Titik (Font Size): {previewFontSize?.toFixed(0)}
-      </ThemedText>
-      <ThemedSlider
-        minimumValue={15}
-        maximumValue={30}
-        value={previewFontSize}
-        onValueChange={(value) => setPreviewFontSize(value)}
-        onSlidingComplete={(value) => setFontSize(value)}
-      />
-      <ThemedText>
-        Taas ng boses (Voice Pitch): {speechPitch?.toFixed(1)}
-      </ThemedText>
-      <ThemedSlider
-        minimumValue={0.1}
-        maximumValue={2}
-        step={0.1}
-        value={speechPitch}
-        onValueChange={(value) => setSpeechPitch(value)}
-      />
-      <ThemedText>
-        Bilis ng pag-basa (Voice Rate): {speechRate?.toFixed(1)}
-      </ThemedText>
-      <ThemedSlider
-        minimumValue={0.1}
-        maximumValue={2}
-        step={0.1}
-        value={speechRate}
-        onValueChange={(value) => setSpeechRate(value)}
-      />
-      <ThemedText>
-        Tema (Theme Mode)
-      </ThemedText>
-      <ThemedRadioButton
-        options={[
-          { value: 'light', label: 'Maliwanag' },
-          { value: 'dark', label: 'Madilim' },
-          { value: 'auto', label: 'Awtomatiko' },
-        ]}
-        value={themeMode}
-        onValueChange={(value) => setThemeMode(value)}
-      />
-      <TouchableOpacity onPress={onTryToRead} style={[styles.button, { backgroundColor: !isReading ? '#28a745' : '#dc3545', marginTop: 'auto' }]}>
-        <ThemedText style={styles.buttonText}>{isReading ? 'Huminto sa pag-basa (Stop reading)' : 'Subukan ang pag-babasa (Test reading)'}</ThemedText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onResetSetting} style={[styles.button]}>
-        <ThemedText style={styles.buttonText}>I-reset ang Mga Setting (Reset Settings)</ThemedText>
-      </TouchableOpacity>
+        <ThemedSlider
+          minimumValue={15}
+          maximumValue={30}
+          value={previewFontSize}
+          onValueChange={(value) => setPreviewFontSize(value)}
+          onSlidingComplete={(value) => setFontSize(value)}
+        />
+        <ThemedText>
+          Taas ng boses (Voice Pitch): {speechPitch?.toFixed(1)}
+        </ThemedText>
+        <ThemedSlider
+          minimumValue={0.6}
+          maximumValue={1.5}
+          step={0.1}
+          value={speechPitch}
+          onValueChange={(value) => setSpeechPitch(value)}
+        />
+        <ThemedText>
+          Bilis ng pag-basa (Voice Rate): {speechRate?.toFixed(1)}
+        </ThemedText>
+        <ThemedSlider
+          minimumValue={0.6}
+          maximumValue={1.5}
+          step={0.1}
+          value={speechRate}
+          onValueChange={(value) => setSpeechRate(value)}
+        />
+        <ThemedText>
+          Tema (Theme Mode)
+        </ThemedText>
+        <ThemedRadioButton
+          options={[
+            { value: 'light', label: 'Maliwanag' },
+            { value: 'dark', label: 'Madilim' },
+            { value: 'auto', label: 'Awtomatiko' },
+          ]}
+          value={themeMode}
+          onValueChange={(value) => setThemeMode(value)}
+        />
+        <TouchableOpacity onPress={onTryToRead} style={[styles.button, { backgroundColor: !isReading ? '#28a745' : '#dc3545', marginTop: 25 }]}>
+          <ThemedText style={styles.buttonText}>{isReading ? 'Huminto sa pag-basa (Stop reading)' : 'Subukan ang pag-babasa (Test reading)'}</ThemedText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onResetSetting} style={[styles.button]}>
+          <ThemedText style={styles.buttonText}>I-reset ang Mga Setting (Reset Settings)</ThemedText>
+        </TouchableOpacity>
+      </ScrollView>
     </ThemedContainer>
   )
 }
